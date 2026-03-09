4dede8c2b446c71f8133b887bc527f2ba08f837c
BerkeBoran <2022141046@cumhuriyet.edu.tr>
2026-03-09 16:58:23 +0300

    refactor(admin): cleanup model admin forms using exclude

--- apps/courses/admin.py
+++ apps/courses/admin.py
@@ -8,27 +8,34 @@ from apps.courses.models.curriculum import Curriculum,CurriculumDetail
 @admin.register(Classroom)
 class ClassroomAdmin(admin.ModelAdmin):
     list_display = ("classroom_name", "classroom_capacity")
+    exclude = ("is_deleted","deleted_at")
 
 @admin.register(CourseTime)
 class CourseTimeAdmin(admin.ModelAdmin):
     list_display = ("course_start_time","course_end_time","course_days")
+    exclude = ("is_deleted","deleted_at")
 
 @admin.register(Grade)
 class GradeAdmin(admin.ModelAdmin):
     list_display = ("grade",)
+    exclude = ("is_deleted","deleted_at")
 
 
 @admin.register(Department)
 class DepartmentAdmin(admin.ModelAdmin):
     list_display = ("department",)
+    exclude = ("is_deleted","deleted_at")
 
 @admin.register(Semester)
 class SemesterAdmin(admin.ModelAdmin):
     list_display = ("semester","is_active")
+    exclude = ("is_deleted","deleted_at")
 
 @admin.register(Course)
 class CoursesAdmin(admin.ModelAdmin):
     list_display = ("ects","credit","course_name","course_id","course_detail",)
+    filter_horizontal = ("prerequisites",)
+    exclude = ("is_deleted","deleted_at")
 
 @admin.register(Enrollment)
 class EnrollmentAdmin(admin.ModelAdmin):
@@ -37,6 +44,7 @@ class EnrollmentAdmin(admin.ModelAdmin):
 @admin.register(Section)
 class SectionAdmin(admin.ModelAdmin):
     list_display = ("course","semester","classroom","instructor","capacity","course_time")
+    exclude = ("is_deleted",)
 
 class CurriculumDetailInline(admin.TabularInline):
     model = CurriculumDetail
--- apps/users/admin.py
+++ apps/users/admin.py
@@ -5,12 +5,12 @@ from apps.users.models.instructor import Instructor
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ("username", "first_name", "last_name", "enrollment_date","grade", "department")
-    exclude = ("password","last_login","groups", "is_superuser", "is_staff", "is_active", "date_joined","user_permissions")
+    exclude = ("password","last_login","groups", "is_superuser", "is_staff", "is_active", "date_joined","user_permissions","section","deleted_at","is_deleted","courses")
     readonly_fields = ("username","role")
 
 @admin.register(Instructor)
 class InstructorAdmin(admin.ModelAdmin):
     list_display = ("username", "first_name", "last_name", "title", "department")
-    exclude = ("password","last_login","is_superuser", "is_staff", "is_active", "date_joined","user_permissions","groups",)
+    exclude = ("password","last_login","is_superuser", "is_staff", "is_active", "date_joined","user_permissions","groups","is_deleted","deleted_at")
     readonly_fields = ("username","role")
 
